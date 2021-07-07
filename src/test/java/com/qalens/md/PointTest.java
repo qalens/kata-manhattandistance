0bdcb919c9b7a0a622ea76e83a2b1ef1ac3289ec
Naik <atm@Naiks-MacBook-Air.local>
2021-07-07 20:15:49 +0530

    ATM | added tests to cover more cases

--- src/test/java/com/qalens/md/PointTest.java
+++ src/test/java/com/qalens/md/PointTest.java
@@ -2,12 +2,28 @@ package com.qalens.md;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PointTest {
-    @Test
-    void shouldCalculateHorizontalDistanceProperly(){
-        Point p1=new Point(3,0);
-        Point p2=new Point(6,0);
-        Assertions.assertEquals(3,p1.distanceFrom(p2));
+    @ParameterizedTest
+    @CsvSource({
+            "3,6,0,3",
+            "3,6,-1,3",
+            "3,6,1,3",
+            "-3,6,0,9",
+            "-3,6,-1,9",
+            "-3,6,1,9",
+            "-3,-6,0,3",
+            "-3,-6,-1,3",
+            "-3,-6,1,3",
+            "3,-6,0,9",
+            "3,-6,-1,9",
+            "3,-6,1,9"
+    })
+    void shouldCalculateHorizontalDistanceProperly(int x1,int x2,int y,int expected){
+        Point p1=new Point(x1,y);
+        Point p2=new Point(x2,y);
+        Assertions.assertEquals(expected,p1.distanceFrom(p2));
     }
 }
