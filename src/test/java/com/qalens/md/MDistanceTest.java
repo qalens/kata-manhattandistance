56e1bcc185089307c6f869d175bfc1c12e988460
Naik <atm@Naiks-MacBook-Air.local>
2021-07-07 20:24:23 +0530

    ATM | made test for distance function more generic

--- src/test/java/com/qalens/md/MDistanceTest.java
+++ src/test/java/com/qalens/md/MDistanceTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 public class MDistanceTest {
     @Test
     void shouldCalculateHorizontalDistanceProperly(){
-        Point p1=new Point(3,0);
-        Point p2=new Point(6,0);
-        Assertions.assertEquals(3,MDistance.manhattanDistance(p1,p2));
+        Point p1=new Point(3,3);
+        Point p2=new Point(6,6);
+        Assertions.assertEquals(6,MDistance.manhattanDistance(p1,p2));
     }
 }
