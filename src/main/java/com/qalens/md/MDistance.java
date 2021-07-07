4448dfb472897c7343abbc8f75815847c9216d42
Naik <atm@Naiks-MacBook-Air.local>
2021-07-07 20:06:40 +0530

    ATM | created project along with filing test

---
+++ src/main/java/com/qalens/md/MDistance.java
@@ -0,0 +1,7 @@
+package com.qalens.md;
+
+public class MDistance {
+    static int manhattanDistance(Point a, Point b){
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
