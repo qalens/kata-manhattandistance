cce50a4acf83db31d227581789bcf0d8b3d30c70
Naik <atm@Naiks-MacBook-Air.local>
2021-07-07 20:17:11 +0530

    ATM | added failing tests for y

--- src/test/java/com/qalens/md/PointTest.java
+++ src/test/java/com/qalens/md/PointTest.java
@@ -26,4 +26,24 @@ public class PointTest {
         Point p2=new Point(x2,y);
         Assertions.assertEquals(expected,p1.distanceFrom(p2));
     }
+    @ParameterizedTest
+    @CsvSource({
+            "3,6,0,3",
+            "3,6,-1,3",
+            "3,6,1,3",
+            "-3,6,0,9",
+            "-3,6,-1,9",
+            "-3,6,1,9",
+            "-3,-6,0,3",
+            "-3,-6,-1,3",
+            "-3,-6,1,3",
+            "3,-6,0,9",
+            "3,-6,-1,9",
+            "3,-6,1,9"
+    })
+    void shouldCalculateVerticalDistanceProperly(int y1,int y2,int x,int expected){
+        Point p1=new Point(x,y1);
+        Point p2=new Point(x,y2);
+        Assertions.assertEquals(expected,p1.distanceFrom(p2));
+    }
 }
